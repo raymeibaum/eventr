4dcd3fb43b5a2afa04d00e6abcd71d93a1efaef3
aylinmarie <aylinmariedesigns@gmail.com>
2017-05-03 17:37:24 -0400

    show user content on profile page

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   def show
-    @user = User.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
+    @user = current_user
 
 		@user ? (render json: @user) :
 		(render json: {status: 404, message: "User not found."})
