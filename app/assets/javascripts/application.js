d2c9da6f7eea4ac28af46ffbf0248dd2959dbad0
Ray Meibaum <rmeibaum@me.com>
2017-05-03 12:01:05 -0400

    add angular

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require angular/angular
+//= require angular-ui-router/release/angular-ui-router
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
+	root to: 'client#index'
+	get '*path', to: 'client#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
