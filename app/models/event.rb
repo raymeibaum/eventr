f9ec180b70e97e713d5c554df24f635f1dd131cb
Ray Meibaum <rmeibaum@me.com>
2017-05-03 17:43:07 -0400

    Events#index loading events from api

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,9 @@ class Event < ApplicationRecord
 
 	has_many :comments
 	has_many :users, through: :comments
+
+	def self.get_events(location = "Atlanta", date = "today")
+		events = HTTParty.get("http://api.eventful.com/json/events/search?app_key=vg3wPtKcvHhssZxh&location=#{location}&date=#{date}")
+		parsed = JSON.parse(events)["events"]["event"]
+	end
 end
