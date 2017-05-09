8eae586834e0a01156a1670c9969c072ccd2aae6
Ray Meibaum <rmeibaum@me.com>
2017-05-09 16:05:01 -0400

    add comments and favorites methods to event and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ApplicationRecord
 
 	has_many :actions
 	has_many :events, through: :actions
+
+	def comments
+		comments = []
+		actions.where(activity_type: "Comment").each do |action|
+			comments << action.activity
+		end
+		comments
+	end
+
+	def favorites
+		favorites = []
+		actions.where(activity_type: "Favorite").each do |action|
+			favorites << action.activity
+		end
+		favorites
+	end
 end
