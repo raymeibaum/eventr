e0f1f41288e4c48d071652fcdd5cf94efe96e6ad
Ray Meibaum <rmeibaum@me.com>
2017-05-09 11:24:03 -0400

    models done, favoriting works, schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509135556) do
+ActiveRecord::Schema.define(version: 20170509152036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20170509135556) do
   create_table "comments", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.integer  "eventUser_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "event_user_id"
   end
 
   create_table "event_users", force: :cascade do |t|
