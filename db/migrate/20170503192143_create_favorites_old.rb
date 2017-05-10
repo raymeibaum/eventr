577386a23cfb6add353169db98e0459a2b0fe79a
Ray Meibaum <rmeibaum@me.com>
2017-05-10 10:57:45 -0400

    rename migration

--- db/migrate/20170503192143_create_favorites_old.rb
+++ db/migrate/20170503192143_create_favorites_old.rb
@@ -1,4 +1,4 @@
-class CreateFavorites < ActiveRecord::Migration[5.0]
+class CreateFavoritesOld < ActiveRecord::Migration[5.0]
   def change
     create_table :favorites do |t|
       t.integer :event_id
