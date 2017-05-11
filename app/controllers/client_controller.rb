04deda72a9553930e713560d8ea9c331a7b3ff95
aylinmarie <aylinmariedesigns@gmail.com>
2017-05-11 12:49:33 -0400

    update about view

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,7 +1,9 @@
 class ClientController < ApplicationController
   layout false
   layout 'application', :except => [:home, :about]
-  skip_before_action :authenticate_user!, only: [:home, :about]
+  skip_before_action :authenticate_user!, :only => [:home]
+  skip_before_action :authenticate_user!, :only => [:about]
+
 
   def index
   end
