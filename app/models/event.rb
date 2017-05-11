477395cc15e97b8b4916d25021cacc51d48a4dc0
Ray Meibaum <rmeibaum@me.com>
2017-05-10 20:04:06 -0400

    update event show

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,18 +17,18 @@ class Event < ApplicationRecord
 		parsed = JSON.parse(categories)["category"]
 	end
 
-	def self.load_event(id)
-		event = HTTParty.get("http://api.eventful.com/json/events/get?app_key=vg3wPtKcvHhssZxh&id=#{id}")
-		parsed = JSON.parse(event)
-		if parsed
-			return parsed
-		else
-			return nil
-		end
-	end
+	# def self.load_event(id)
+	# 	event = HTTParty.get("http://api.eventful.com/json/events/get?app_key=vg3wPtKcvHhssZxh&image_sizes=large&id=#{id}")
+	# 	parsed = JSON.parse(event)
+	# 	if parsed
+	# 		return parsed
+	# 	else
+	# 		return nil
+	# 	end
+	# end
 
 	def self.get_event(id)
-		event = HTTParty.get("http://api.eventful.com/json/events/get?app_key=vg3wPtKcvHhssZxh&id=#{id}")
+		event = HTTParty.get("http://api.eventful.com/json/events/get?app_key=vg3wPtKcvHhssZxh&image_sizes=large&id=#{id}")
 		parsed = JSON.parse(event)
 		if parsed
 			return parsed
